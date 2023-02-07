e5c96843f2cf3d5d0e107048ff3bc7e151d81be9
tihonovrv <tihonovrv@sovcombank.ru>
2023-02-07 20:07:12 +0300

    feat: add styles, themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import React, {Suspense} from 'react';
-import {Routes, Route, Link} from 'react-router-dom'
-import './index.scss'
+import React, {Suspense, useContext, useState} from "react";
+import {Routes, Route, Link} from "react-router-dom";
+import "./styles/index.scss";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
+
+    // const {theme, setTheme} = useContext(ThemeContext)
+    // const toggleTheme = () => {
+    //     setTheme(theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT)
+    // }
+
     return (
-        <div className="app">
-            <Link to={'/about'}>О сайте</Link>
-            <Link to={'/'}>Главная</Link>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle Theme</button>
+            <Link to={"/about"}>О сайте</Link>
+            <Link to={"/"}>Главная</Link>
             <Suspense fallback={<div>Загрузка ...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={"/about"} element={<AboutPageAsync/>}/>
+                    <Route path={"/"} element={<MainPageAsync/>}/>
                 </Routes>
             </Suspense>
         </div>
