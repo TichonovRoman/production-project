090a8f957381b7d90451e8195ea68cfedd213eb3
tihonovrv <tihonovrv@sovcombank.ru>
2023-02-08 19:17:33 +0300

    feat: add mine classnames

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,14 @@ import "./styles/index.scss";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
-
-
-    // const {theme, setTheme} = useContext(ThemeContext)
-    // const toggleTheme = () => {
-    //     setTheme(theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT)
-    // }
+    console.log(classNames("remove-btn", {hovered: false, selectable: true, red: true}, ["pdg"]))
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Toggle Theme</button>
             <Link to={"/about"}>О сайте</Link>
             <Link to={"/"}>Главная</Link>
