ba6181e54a5ca34aa538daf91df71ed9e0d999d7
TichonovRoman <romikt@inbox.ru>
2023-02-09 22:07:01 +0300

    ui kit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import {Link} from "react-router-dom";
 import "./styles/index.scss";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
         <div className={classNames("app", {}, [theme])}>
+            <Navbar/>
             <button onClick={toggleTheme}>Toggle Theme</button>
-            <Link to={"/about"}>О сайте</Link>
-            <Link to={"/"}>Главная</Link>
             <AppRouter/>
         </div>
     );
