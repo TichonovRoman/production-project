0587f88126c28326d6d602e28360eaa0e2bef6e1
tihonovrv <tihonovrv@sovcombank.ru>
2023-03-20 18:47:47 +0300

    fix tests

--- src/widgets/SideBar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/SideBar/ui/Sidebar/Sidebar.test.tsx
@@ -1,14 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from './Sidebar';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
 
 describe('Sidebar', () => {
   test('with have class', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
   test('test toggle', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     // fireEvent - по нему можем подцепиться к событиям
