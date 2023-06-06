c4d3c775ace797836e9aa4f545efb6613bb5f9e7
tihonovrv <tihonovrv@sovcombank.ru>
2023-06-06 18:49:42 +0300

    add tests for login module

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,18 +1,18 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-import { User, userActions } from 'entities/User';
-import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
+import axios from "axios";
+import {createAsyncThunk} from "@reduxjs/toolkit";
 
+import {USER_LOCALSTORAGE_KEY} from "shared/const/localstorage";
+import {User, userActions} from "entities/User";
 interface LoginByUsernameProps {
     username: string;
     password: string;
 }
 
 export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { rejectValue: string }>(
-    'login/loginByUsername',
+    "login/loginByUsername",
     async (authData, thunkAPI) => {
         try {
-            const response = await axios.post<User>('http://localhost:8000/login', authData);
+            const response = await axios.post<User>("http://localhost:8000/login", authData);
 
             if (!response.data) {
                 throw new Error();
@@ -24,7 +24,7 @@ export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { re
             return response.data;
         } catch (e) {
             console.log(e);
-            return thunkAPI.rejectWithValue('error');
+            return thunkAPI.rejectWithValue("error");
         }
     },
 );
