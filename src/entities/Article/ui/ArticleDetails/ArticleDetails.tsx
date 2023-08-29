a6bceaa103bdab15514b001ed35cae1d96aaea3f
tihonovrv <tihonovrv@sovcombank.ru>
2023-08-29 19:17:56 +0300

    article details 7:10

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -12,9 +12,12 @@ import {
     getArticleDetailsError,
     getArticleDetailsIsLoading
 } from "../../model/selectors/articleDetails";
-import {TextAlign, Text} from "shared/ui/Text/Text";
+import {Text, TextAlign, TextSize} from "shared/ui/Text/Text";
 import {Skeleton} from "shared/ui/Skeleton/Skeleton";
 import {Avatar} from "shared/ui/Avatar/Avatar";
+
+import EyeIcon from "shared/assets/icons/eye.svg";
+import CalendarIcon from "shared/assets/icons/CalendarIcon.svg";
 
 interface ArticleDetailsPropsType {
     className?: string;
@@ -35,31 +38,47 @@ export const ArticleDetails = memo(({className, id}: ArticleDetailsPropsType) =>
 
     useEffect(() => {
         dispatch(fetchArticleById(id))
-    },[dispatch, id])
+    }, [dispatch, id])
 
     let content;
 
-    if(isLoading) {
+    if (isLoading) {
         content = (
             <>
-                <Skeleton className={cls.avatar} width={200} height={200} border="50%" />
-                <Skeleton className={cls.title} width={300} height={32} />
-                <Skeleton className={cls.skeleton} width={600} height={24} />
-                <Skeleton className={cls.skeleton} width="100%" height={200} />
-                <Skeleton className={cls.skeleton} width="100%" height={200} />
+                <Skeleton className={cls.avatar} width={200} height={200} border="50%"/>
+                <Skeleton className={cls.title} width={300} height={32}/>
+                <Skeleton className={cls.skeleton} width={600} height={24}/>
+                <Skeleton className={cls.skeleton} width="100%" height={200}/>
+                <Skeleton className={cls.skeleton} width="100%" height={200}/>
             </>
         )
-    } else if(error) {
+    } else if (error) {
         content = (
             <Text
                 align={TextAlign.CENTER}
-                title={t('Произошла ошибка при загрузке статьи')}/>
+                title={t("Произошла ошибка при загрузке статьи")}/>
         )
     } else {
         content = (
             <>
-                <Avatar size={200} src={article?.img} className={cls.avatar}/>
-                <Text title={article?.title} text={article?.subtitle}/>
+                <div className={cls.avatarWrapper}>
+                    <Avatar size={200} src={article?.img} className={cls.avatar}/>
+                </div>
+
+                <Text
+                    className={cls.title}
+                    title={article?.title}
+                    text={article?.subtitle}
+                    size={TextSize.L}
+                />
+                <div className={cls.articleInfo}>
+                    <EyeIcon/>
+                    <Text text={String(article?.views)}/>
+                </div>
+                <div className={cls.articleInfo}>
+                    <CalendarIcon/>
+                    <Text text={article?.createdAt}/>
+                </div>
             </>
         )
     }
