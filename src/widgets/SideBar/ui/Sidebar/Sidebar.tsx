59dbd77c15876589fc011aeb05f0f82f6cf839a6
tihonovrv <tihonovrv@sovcombank.ru>
2023-09-14 19:10:14 +0300

    fix sidebar (добавление id в url из селектора)

--- src/widgets/SideBar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/SideBar/ui/Sidebar/Sidebar.tsx
@@ -1,37 +1,38 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { memo, useMemo, useState } from 'react';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
-import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
-import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
-import { useSelector } from 'react-redux';
-import { getUserAuthData } from 'entities/User';
-import cls from './Sidebar.module.scss';
-import { SidebarItemsList } from '../../model/items';
-import { SidebarItem } from '../SidebarItem/SidebarItem';
+import {classNames} from "shared/lib/classNames/classNames";
+import {memo, useMemo, useState} from "react";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {LangSwitcher} from "shared/ui/LangSwitcher/LangSwitcher";
+import {Button, ButtonSize, ButtonTheme} from "shared/ui/Button/Button";
+import {useSelector} from "react-redux";
+import {getUserAuthData} from "entities/User";
+import cls from "./Sidebar.module.scss";
+import {SidebarItem} from "../SidebarItem/SidebarItem";
+import {getSidebarItems} from "../../model/selectors/getSidebarItems";
 
 interface SidebarProps {
     className?: string;
 }
 
-export const Sidebar = memo(({ className }: SidebarProps) => {
+export const Sidebar = memo(({className}: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const sidebarItemList = useSelector(getSidebarItems)
 
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
 
-    const itemsList = useMemo(() => SidebarItemsList.map((item) => (
+    const itemsList = useMemo(() => sidebarItemList.map((item) => (
         <SidebarItem
             item={item}
             collapsed={collapsed}
             key={item.path}
         />
-    )), [collapsed]);
+    )), [collapsed, sidebarItemList]);
 
     return (
         <div
             data-testid="sidebar"
-            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+            className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}
         >
             <Button
                 data-testid="sidebar-toggle"
@@ -41,13 +42,13 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
                 size={ButtonSize.L}
                 square
             >
-                {collapsed ? '>' : '<'}
+                {collapsed ? ">" : "<"}
             </Button>
             <div className={cls.items}>
                 {itemsList}
             </div>
             <div className={cls.switchers}>
-                <ThemeSwitcher />
+                <ThemeSwitcher/>
                 <LangSwitcher
                     short={collapsed}
                     className={cls.lang}
