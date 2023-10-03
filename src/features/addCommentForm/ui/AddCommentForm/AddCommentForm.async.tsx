6ea2c89797f8924a6889bd48d64a49be2043d225
TichonovRoman <romikt@inbox.ru>
2023-10-03 19:05:31 +0300

    fix error build prod

--- src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
+++ src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
@@ -1,7 +1,4 @@
 import {FC, lazy} from "react";
 import {AddCommentFormPropsType} from "./AddCommentForm"
 
-export const AddCommentFormAsync = lazy<FC<AddCommentFormPropsType>>(() => new Promise((resolve) => {
-    // @ts-ignore
-    setTimeout(() => resolve(import("./AddCommentForm")), 1500);
-}));
+export const AddCommentFormAsync = lazy<FC<AddCommentFormPropsType>>(() => import("./AddCommentForm"));
