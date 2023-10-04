65ab0128d671fc728ed8a3fe07236fbf55ce9ca3
TichonovRoman <romikt@inbox.ru>
2023-10-04 19:29:07 +0300

    deploy server 3

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,10 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
-import {Sidebar} from "widgets/Sidebar";
+// import {Sidebar} from "widgets/Sidebar";
 import {useDispatch, useSelector} from "react-redux";
 import {getUserInited, userActions} from "entities/User";
+import {Sidebar} from "../widgets/Sidebar/ui/Sidebar/Sidebar";
 
 function App() {
     const {theme} = useTheme();
