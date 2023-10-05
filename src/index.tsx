ad2f439f54568acc189ac3111897d61b39e9327e
TichonovRoman <romikt@inbox.ru>
2023-10-05 18:50:35 +0300

    add netlify config 10

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { StoreProvider } from 'app/providers/StoreProvider';
 import 'app/styles/index.scss';
 import './shared/config/i18n/i18n';
 import { ErrorBoundary } from './app/providers/ErrorBoundary';
-import App from "app/App";
+import App from "App";
 
 render(
     <BrowserRouter>
