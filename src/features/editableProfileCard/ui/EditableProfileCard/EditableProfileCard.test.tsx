32c1a77e45fad3e0a869be3347a10d740951e964
TichonovRoman <romikt@inbox.ru>
2023-11-07 19:26:55 +0300

    add use event, test

--- src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.test.tsx
+++ src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.test.tsx
@@ -1,11 +1,44 @@
 import {fireEvent, screen} from "@testing-library/react";
 import {EditableProfileCard} from "./EditableProfileCard";
 import {componentRender} from "shared/lib/tests/componentRender/componentRender";
+import {Profile} from "entities/Profile";
+import {Currency} from "entities/Currency";
+import {Country} from "entities/Country";
+import {profileReducer} from "../../model/slice/profileSlice";
+import {userEvent} from "@testing-library/user-event/setup/index";
+
+const profile: Profile = {
+    id: "1",
+    first: "admin",
+    lastname: "adminLastName",
+    age: 465,
+    currency: Currency.USD,
+    country: Country.Kazakhstan,
+    city: "Moscow",
+    username: "admin123",
+}
+
 
 describe("features/EditableProfileCard", () => {
-    test("with only first param", () => {
-        componentRender(<EditableProfileCard />);
-        expect(screen.getByTestId("sidebar")).toBeInTheDocument();
+    test("Режим ридонли должен переключиться", () => {
+        componentRender(<EditableProfileCard />, {
+            initialState: {
+                profile: {
+                    readonly: true,
+                    data: profile,
+                    form: profile
+                }
+            },
+            asyncReducers: {profile: profileReducer}
+        });
+
+        expect(screen.getByTestId("ProfileCard.firstName")).toBeInTheDocument();
+        expect(screen.getByTestId("ProfileCard.lastName")).toBeInTheDocument();
+
+        userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'));
+        expect(screen.getByTestId("EditableProfileCardHeader.CancelButton")).toBeInTheDocument()
+        // expect(screen.getByTestId("EditableProfileCardHeader.EditButton")).toBeInTheDocument()
+
     });
 
 });
