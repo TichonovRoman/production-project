88a90cbed5d2a74087de3c2624472ae55ef2b32a
TichonovRoman <romikt@inbox.ru>
2023-12-11 18:09:17 +0300

    add vite for development mode

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -8,7 +8,6 @@ import NotificationIcon from "@/shared/assets/icons/notification.svg";
 import {NotificationList} from "@/entities/Notification";
 import {Drawer} from "@/shared/ui/Drawer/Drawer";
 import {BrowserView, MobileView} from "react-device-detect";
-import {AnimationProvider} from "@/shared/lib/components/AnimationProvider";
 
 interface NotificationButtonPropsType {
     className?: string
