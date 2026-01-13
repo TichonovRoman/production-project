e306907fe42c54a6f9e6c34b7c663a172f7db1bc
Roman Tichonov <tichonovrv@sovcombank.ru>
2026-01-13 21:32:38 +0300

    112: fix problems

--- src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
+++ src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import {ComponentStory, ComponentMeta} from "@storybook/react";
-// @ts-ignore
-import withMock from "storybook-addon-mock";
 
 import {NotificationList} from "./NotificationList";
 import {StoreDecorator} from "@/shared/config/storybook/StoreDecorator/StoreDecorator";
@@ -11,8 +9,7 @@ export default {
     component: NotificationList,
     argTypes: {
         backgroundColor: {control: "color"},
-    },
-    decorators: [withMock]
+    }
 } as ComponentMeta<typeof NotificationList>;
 
 const Template: ComponentStory<typeof NotificationList> = (args) => <NotificationList {...args} />;
