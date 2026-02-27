f971fb0f1f749d27e22cfdff8cced4bda5de30f6
Roman Tichonov <tichonovrv@sovcombank.ru>
2026-02-27 19:47:58 +0300

    115: add slip. fixtures

--- cypress/e2e/article/articles-list.cy.ts
+++ cypress/e2e/article/articles-list.cy.ts
@@ -8,4 +8,16 @@ describe('Пользователь заходит на страницу со с�
         cy.getByTestId('ArticleList').should('exist');
         cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
     });
+
+    it('На стабах', () => {
+        cy.intercept('GET', '**/articles?', {fixture: 'articles.json'})
+        cy.getByTestId('ArticleList').should('exist');
+        cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
+    });
+
+    it.skip('Пример заскипанного теста', () => {
+        cy.getByTestId('ArticleList').should('exist');
+        cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
+        cy.get('fggffg').should('exist');
+    });
 });
